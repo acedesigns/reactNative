d21d1c685e4739709e1331720c09b63db6df1639
Ace Designs <acedesigns123@gmail.com>
2021-04-05 22:41:35 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/* =======================================================
+ *
+ * Created by anele on 2021/04/05.
+ *
+ * @anele_ace
+ *
+ * =======================================================
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
