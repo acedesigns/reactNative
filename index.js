2235b9dad231f656812df448aa3a46a543841103
Ace Designs <acedesigns123@gmail.com>
2021-04-05 23:06:53 +0200

    Third Part dependencies

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-/* =======================================================
- *
- * Created by anele on 2021/04/05.
- *
- * @anele_ace
- *
- * =======================================================
- */
-
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
