aea7f6772567e078528584644989571cefc2a7e5
Ace Designs <acedesigns123@gmail.com>
2022-02-09 16:31:26 +0200

    added new dependencies

--- App.tsx
+++ App.tsx
@@ -20,60 +20,48 @@ import AuthStackNavigator from './src/navigators/AuthStackNavigator';
 import DrawerStackNavigator from './src/navigators/appDrawerNavigator';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState<boolean | null>(true);
+  const [userToken, setUserToken] = useState<string | null>(null);
 
-     const [isLoading, setIsLoading] = useState(true);
-     const [userToken, setUserToken] = useState(null);
+  const authContext = useMemo(
+    () => ({
+      logInMethod: () => {
+        setUserToken('abc');
+        setIsLoading(false);
+      },
+      logOutMethod: () => {
+        setUserToken(null);
+        setIsLoading(false);
+      },
+      registerMethod: () => {
+        setUserToken('abc');
+        setIsLoading(false);
+      },
+    }),
+    [],
+  );
 
-     const authContext = useMemo(() => ( {
-         logInMethod: () => {
-             setUserToken('abc');
-             setIsLoading(false);
-         },
-         logOutMethod: () => {
-             setUserToken(null);
-             setIsLoading(false);
-         },
-         registerMethod: () => {
-             setUserToken('abc');
-             setIsLoading(false);
-         },
-     } ) );
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+  }, []);
 
-     useEffect(() => {
-         setTimeout(() => {
-             setIsLoading(false);
-         }, 5000)
-     }, []);
+  if (isLoading) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+  return (
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        {userToken !== null ? <DrawerStackNavigator /> : <AuthStackNavigator />}
+      </NavigationContainer>
+    </AuthContext.Provider>
+  );
+};
 
-     if (isLoading) {
-         return (
-             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                 <ActivityIndicator size="large" />
-             </View>
-         )
-     }
-     return (
-
-         <AuthContext.Provider value={authContext}>
-             <NavigationContainer>
-                 { userToken !== null ?
-                     ( <DrawerStackNavigator /> )
-                     :  <AuthStackNavigator />
-                 }
-             </NavigationContainer>
-         </AuthContext.Provider>
-     )
- };
-
- const styles = StyleSheet.create({
-     container: {
-		flex: 1,
-	 },
-     textScreen: {
-         flex: 1,
-         alignItems: 'center',
-         justifyContent: 'center'
-     }
- });
 
 export default App;
